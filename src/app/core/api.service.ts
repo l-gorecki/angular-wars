fa3545982835b4ed97a0c428b23b32f8bdd20c69
l-gorecki <gorecki.lukas@gmail.com>
2017-12-25 06:25:02 +0100

    improved service, added typing for responses

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as entity from './core.interface';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ApiService {
-
+  private apiURL = 'https://swapi.co/api/';
   constructor(private http: HttpClient) { }
 
-  getPath(path: String) {
-    return this.http.get('https://swapi.co/api/');
+  getRoot() {
+    return this.http.get(this.apiURL).map(r => {
+      let data: any[];
+      data = Object.entries(r);
+      return data;
+    });
   }
 
+  getResourceByType(type: String): Observable<Response> {
+    return this.http.get<Response>(this.apiURL + type).map((res: Response) => res);
+  }
+}
+
+
+interface Response {
+  count: number;
+  next: string;
+  previous: string;
+  results: entity.Person | entity.Vehicle | entity.Film | entity.Planet | entity.Species | entity.Starship;
 }
---
+++ src/app/core/core.interface.ts
@@ -0,0 +1,87 @@
+import { Species } from './core.interface';
+export interface Person {
+    birth_year: string;
+    eye_color: string;
+    films: any[]; // zmienic typ potem
+    gender: string;
+    hair_color: string;
+    height: number;
+    mass: number;
+    homeworld: any;
+    name: string;
+    species: any;
+    starships: any;
+    vehicles: any;
+}
+export interface Vehicle {
+  cargo_capacity: number;
+  consumables: string;
+  cost_in_credits: number;
+  crew: number;
+  hair_color: string;
+  length: number;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  name: string;
+  model: string;
+  passengers: number;
+  pilots: any[];
+  vehicle_class: string;
+}
+export interface Planet {
+  climate: string;
+  diameter: number;
+  gravity: string;
+  films: any[];
+  name: string;
+  orbital_period: number;
+  population: number;
+  residents: any[];
+  rotation_period: number;
+  surface_water: number;
+  terrain: string;
+}
+export interface Species {
+  average_height: number;
+  average_lifespan: number;
+  classification: string;
+  designation: string;
+  eye_colors: string;
+  hair_colors: string;
+  films: any[];
+  homeworld: any;
+  language: string;
+  name: string;
+  people: any[];
+  skin_color: string;
+}
+export interface Film {
+  characters: any[];
+  director: string;
+  episode_id: number;
+  opening_crawl: string;
+  planets: any[];
+  producer: string;
+  release_date: string;
+  species: any[];
+  starships: any[];
+  title: string;
+  vehicles: any[];
+}
+export interface Starship {
+  MGLT: number;
+  cargo_capacity: number;
+  consumables: string;
+  cost_in_credits: number;
+  crew: number;
+  films: any[];
+  hyperdrive_rating: number;
+  length: number;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: number;
+  pilots: any[];
+  starship_class: string;
+}
